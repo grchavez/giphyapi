dd4418383cfcbef37c2d5fb3c5f0f6e364b07da9
gabe <grchavez@email.arizona.edu>
2017-12-06 12:39:08 -0700

    No errors in code, but not rendering yet.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,11 +1,12 @@
 // Array Containing Initial Choices
-var animes = ["Dragonball Z", "Naruto", "Bleach", "One-Punch Man"];
+var animes = ["Dragonball Z","One-Punch Man", "Attack on Titan", "Sailor Moon"];
 
 // Render Function
 function renderButtons(){
 	$(".anime").empty();
 	for (var i = 0; i < animes.length; i++){
 		var a = $("<button>");
+		a.attr("type","button");
 		a.addClass("btn btn-secondary cateBtn");
 		a.text(animes[i]);
 		a.attr("data-name", animes[i]);
@@ -20,9 +21,53 @@ $(".addAnime").on("click", function(event){
 	event.preventDefault();
 	var anime = $(".anime-input").val().trim();
 	animes.push(anime);
+	console.log(anime);
 	renderButtons();
-
-
 });
+
+// Giphy Function
+
+$(".cateBtn").on("click", function(){
+
+	var input = $(this).attr("data-name");
+
+	var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + input + "3d8Qjg9IrHbxpxvXioeXAgyAk5RNp5MO";
+
+	$.ajax({
+		url: queryURL,
+		method: "GET"
+	})
+	.done(function(response){
+
+		var results = response.data;
+
+		for (var i = 0; i < results.length; i++){
+			if(results[i].rating !== "r" && results[i].rating !== "pg-13"){
+				var gifDiv = $("<div class='card'>");
+				var rating = results[i].rating;
+				var p = $("<p class='card-text'>").text("Rating: " + rating);
+				var gifImage = $("<img class='card-img-top'>");
+				gifImage.attr("src", results[i].images.fixed_height.url);
+				gifDiv.append(p);
+				gifDiv.append(gifImage);
+
+				$(".gifResult").prepend(gifDiv);
+
+
+			}
+		}
+
+
+
+	})
+
+
+
+
+
+
+
+
+})
 
 renderButtons();
